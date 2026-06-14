55acbd0775d85efe920f181e0b6ec0dc85e3078c
Anuj Ghongade <anujghongade4@gmail.com>
2026-06-14 15:26:08 +0530

    Update transactionService.js

--- frontend/src/services/transactionService.js
+++ frontend/src/services/transactionService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API ="https://expense-tracker-backend-cqgv.onrender.com";
+const API ="https://expense-tracker-backend-cqgv.onrender.com/api/transactions";
 
 export const getTransactions =
 async () => {
