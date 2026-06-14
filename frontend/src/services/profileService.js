fdb6ffdf8e4a6df2bcd8b826fa6cf67e4a785941
Anuj Ghongade <anujghongade4@gmail.com>
2026-06-14 15:28:11 +0530

    Update profileService.js

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API =
-"https://expense-tracker-backend-cqgv.onrender.com";
+"https://expense-tracker-backend-cqgv.onrender.com/api/users/profile";
 
 export const getProfile =
 async () => {
