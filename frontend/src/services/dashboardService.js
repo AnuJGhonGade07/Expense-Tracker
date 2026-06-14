1000bd209ef42db71f030262114772ebd0f6bf16
Anuj Ghongade <anujghongade4@gmail.com>
2026-06-14 15:08:30 +0530

    Update dashboardService.js

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/dashboard";
+const API = "https://expense-tracker-backend-cqgv.onrender.com";
 
 export const getDashboardData = async()=>{
 
